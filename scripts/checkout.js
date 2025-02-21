1354878b6c1f2790dd959b196db6e2b2d84a94f3
ShrirajVichare <vichareshriraj000@gmail.com>
2025-02-21 14:56:50 +0530

    Save data and generate HTML for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,5 @@
 import { renderOrdersummary } from "./checkout/orderSummary.js";
+import { renderPayementSummary } from "./checkout/paymentSummary.js";
 
 renderOrdersummary();
+renderPayementSummary();
